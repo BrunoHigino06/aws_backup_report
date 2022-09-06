58fe5b0b5f7a5de470f9913568562579121601c6
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-09-06 03:18:03 -0300

    Update aws_backup_report.py

--- v1/aws_backup_report.py
+++ v1/aws_backup_report.py
@@ -5,7 +5,7 @@ def aws_backup_report():
     status = "FAILED"
     regions = ["sa-east-1"]
     for region in regions:
-        brute_data = os.system('aws backup list-backup-jobs --region '+region+' --by-created-afte '+date+' --output text --query "BackupJobs[*].{AccountId:AccountId,BackupJobId:BackupJobId,StartBy:StartBy,State:State,StatusMessage:StatusMessage,ResourceArn:ResourceArn}"')
+        brute_data = os.system('aws backup list-backup-jobs --region '+region+' --by-created-afte '+date+' --output text --query "BackupJobs[*].{AccountId:AccountId"')
         json_data = json.dumps(brute_data, indent=4, sort_keys=True, default=str)
         refined_data = json.loads(json_data)
         print(refined_data)
