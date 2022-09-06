4659f0664040494cea92c5abbdc3fdbf88bfc395
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-09-06 03:23:55 -0300

    Update aws_backup_report.py

--- v1/aws_backup_report.py
+++ v1/aws_backup_report.py
@@ -7,18 +7,6 @@ def aws_backup_report():
     for region in regions:
         accountId = os.system('aws backup list-backup-jobs --region '+region+' --by-created-afte '+date+' --output text --query "BackupJobs[*].{AccountId:AccountId}"')
         for accountname in accountId:
-
-            organizations = boto3.client('organizations')
-
-            org_response = organizations.describe_account(
-            AccountId=accountId
-            )
-
-            org_json = json.dumps(org_response, indent=4, sort_keys=True, default=str)
-
-            org_database = json.loads(org_json)
-
-            accountname = org_database['Account']['Name']
-            print(accountname)
+            print('test: '+accountname)
 
 aws_backup_report()
