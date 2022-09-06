8483b73ffa60afd8d2a56933e33bdf1ade818a53
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-09-06 03:10:07 -0300

    Update aws_backup_report.py

--- v1/aws_backup_report.py
+++ v1/aws_backup_report.py
@@ -8,6 +8,6 @@ def aws_backup_report():
         brute_data = os.system('aws backup list-backup-jobs --region '+region+' --by-created-afte '+date+' --output json --query "BackupJobs[*].{AccountId:AccountId,BackupJobId:BackupJobId,StartBy:StartBy,State:State,StatusMessage:StatusMessage,ResourceArn:ResourceArn}"')
         json_data = json.dumps(brute_data, indent=4, sort_keys=True, default=str)
         refined_data = json.loads(json_data)
-        print(refined_data['BackupJobs']['AccountId'])
+        print(refined_data['BackupJobs']['*']['AccountId'])
 
 aws_backup_report()
