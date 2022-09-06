74916d38327866a91cf5c4d1fe9cf5f818d5478b
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-09-06 03:08:16 -0300

    Update aws_backup_report.py

--- v1/aws_backup_report.py
+++ v1/aws_backup_report.py
@@ -6,8 +6,11 @@ def aws_backup_report():
     regions = ["us-east-1", "sa-east-1"]
     for region in regions:
         brute_data = os.system('aws backup list-backup-jobs --region '+region+' --by-created-afte '+date+' --output json --query "BackupJobs[*].{AccountId:AccountId,BackupJobId:BackupJobId,StartBy:StartBy,State:State,StatusMessage:StatusMessage,ResourceArn:ResourceArn}"')
-        json_data = json.dumps(brute_data, indent=4, sort_keys=True, default=str)
-        refined_data = json.loads(json_data)
-        print(refined_data['BackupJobs']['AccountId'])
+        try:
+            json_data = json.dumps(brute_data, indent=4, sort_keys=True, default=str)
+            refined_data = json.loads(json_data)
+            print(refined_data['BackupJobs']['AccountId'])
+        except:
+            print('?')
 
 aws_backup_report()
